1490dfc2900fcdffe0f623aa295dcc1ee01cc5e4
Harolt0204 <fharolt@gmail.com>
2025-04-18 08:51:25 -0500

    Add files via upload

---
+++ Part2.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+#include <cstddef> 
+
+class Buffer {
+private:
+    int* data;
+    size_t size;
+
+public:
+    Buffer(size_t s) : size(s), data(new int[s]) {
+        std::cout << "Constructor called\n";
+    }
+
+    ~Buffer() {
+        delete[] data;
+        std::cout << "Destructor called\n";
+    }
+
+    Buffer(const Buffer& other) : size(other.size), data(new int[other.size]) {
+        std::copy(other.data, other.data + other.size, data);
+        std::cout << "Copy constructor called\n";
+    }
+
+    Buffer& operator=(const Buffer& other) {
+        if (this != &other) {
+            delete[] data;
+            size = other.size;
+            data = new int[size];
+            std::copy(other.data, other.data + size, data);
+            std::cout << "Copy assignment called\n";
+        }
+        return *this;
+    }
+
+    Buffer(Buffer&& other) noexcept : data(other.data), size(other.size) {
+        other.data = nullptr;
+        other.size = 0;
+        std::cout << "Move constructor called\n";
+    }
+
+    Buffer& operator=(Buffer&& other) noexcept {
+        if (this != &other) {
+            delete[] data;
+            data = other.data;
+            size = other.size;
+            other.data = nullptr;
+            other.size = 0;
+            std::cout << "Move assignment called\n";
+        }
+        return *this;
+    }
+};
+int main() {
+    Buffer b(10);
+    return 0;
+}
